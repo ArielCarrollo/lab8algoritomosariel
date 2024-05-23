9695b38d132cbf46fa65fd770b713f95676eaecb
ArielCarrollo <150464222+ArielCarrollo@users.noreply.github.com>
2024-05-22 23:15:26 -0500

    ,

--- Assets/GraphController.cs
+++ Assets/GraphController.cs
@@ -8,13 +8,21 @@ public class GraphController : MonoBehaviour
     public TextAsset nodePositiontxt;
     public string[] arrayNodePositions;
     public string[] CurrentNodePositions;
+    public TextAsset nodeConectionstxt;
+    public string[] arrayNodeconections;
+    public string[] currentNodeconections;
     //public SimplyLinkedList<GameObject> Allnodes;
     public List<GameObject> AllNodes;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(nodePositiontxt != null)
+        CreateNodes();
+        CreateConections();
+    }
+    void CreateNodes()
+    {
+        if (nodePositiontxt != null)
         {
             arrayNodePositions = nodePositiontxt.text.Split('\n');
             for (int i = 0; i < arrayNodePositions.Length; i++)
@@ -23,7 +31,22 @@ public class GraphController : MonoBehaviour
                 Vector2 position = new Vector2(float.Parse(CurrentNodePositions[0]), float.Parse(CurrentNodePositions[1]));
                 GameObject tmp = Instantiate(nodeprefab, position, transform.rotation);
                 AllNodes.Add(tmp);
-                //Allnodes.InsertNodeAtEnd(tmp);
+                //Allnodes.InsertNodeAtStart(tmp);
+            }
+        }
+    }
+    void CreateConections()
+    {
+        if (nodeConectionstxt != null)
+        {
+            arrayNodeconections = nodeConectionstxt.text.Split('\n');
+            for(int i = 0; i < arrayNodeconections.Length; i++)
+            {
+                currentNodeconections = arrayNodeconections[i].Split(",");
+                for(int j = 0; j < currentNodeconections.Length; j++)
+                {
+                    AllNodes[i].GetComponent<NodeControl>().AddadjacentNodes(AllNodes[int.Parse(currentNodeconections[j])].GetComponent<NodeControl>());
+                }
             }
         }
     }
---
+++ Assets/NodeControl.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeControl : MonoBehaviour
+{
+    public List<NodeControl> adjacentNode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void AddadjacentNodes(NodeControl node)
+    {
+        adjacentNode.Add(node);
+    }
+}
